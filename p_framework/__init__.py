f0f4497f6f5e8b0584092529954d86495bf84491
Yohann Gabory <yohann@gabory.fr>
2015-04-23 21:56:10 +0200

    introducing basic application and view framework

---
+++ p_framework/__init__.py
@@ -0,0 +1,26 @@
+from werkzeug.wrappers import Request
+from werkzeug.exceptions import HTTPException
+
+
+class Application(object):
+
+    def __init__(self, root_url):
+        self.url = root_url
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def dispatch_request(self, request):
+        url_map = self.url.build_rules()
+        urls = url_map.bind_to_environ(request.environ)
+        try:
+            endpoint, kwargs = urls.match()
+
+            return endpoint().render(request, **kwargs)
+        except HTTPException, e:
+            return e
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
