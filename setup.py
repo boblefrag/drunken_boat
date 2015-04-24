2a4b26cdd4011aa14e6db0e92fbab3ff88e1f6b1
yohann <yohann.gabory@novapost.fr>
2015-04-24 12:39:50 +0200

    Back to development: 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="drunken_boat",
-    version="0.0.1",
+    version='0.0.2.dev0',
     author="Yohann Gabory",
     author_email="yohann@gabory.fr",
     description=("Performance based web framework written in python"),
