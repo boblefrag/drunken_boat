d0dc7c53a210b9c9553831e34d03f3072cc9ddb4
Yohann Gabory <yohann.gabory@novapost.net>
2015-05-03 13:42:47 +0200

    WIP

--- drunken_boat/db/postgresql/fields.py
+++ drunken_boat/db/postgresql/fields.py
@@ -12,9 +12,21 @@ class Field(object):
         result = result[1:]
         return hydrated, result
 
+    def get_select(self):
+        """Return the select representation of the field"""
+        if not self.virtual:
+            return '{}.{}'.format(self.table, self.db_name)
+
+        else:
+            if hasattr(self, "alias"):
+                return self.alias
+        return self.db_name
+
 
 class Related(Field):
+
     virtual = True
+    make_join = True
 
     def __init__(self, join, projection, *args, **kwargs):
         from drunken_boat.db.postgresql.projections import Projection
@@ -33,6 +45,41 @@ Projection instance".format(projection))
 
 class ForeignKey(Related):
     pass
+
+
+class ReverseForeign(Related):
+    make_join = False
+
+    def get_select(self):
+        return
+
+    def extra(self, projection, results):
+        """The extra method will make a request to get all the related
+        object pointed by the ReverseForeign storing them in
+        self.related_object.
+
+        then, when self.hydrate will be called, those objects will be
+        added to the object representation of the main Projection.
+        """
+        lookup = [getattr(result, self.join[0]) for result in results]
+        reverse_projection = self.projection(projection.db)
+        reverses = reverse_projection.select(
+            where="WHERE {}=ANY(%s)".format(self.join[1]),
+            params=[lookup])
+        for result in results:
+            setattr(
+                result,
+                self.name,
+                [reverse for reverse in reverses
+                 if getattr(
+                         reverse,
+                         self.join[1]) == getattr(result, self.join[0])])
+
+        return results
+
+    def hydrate(self, result):
+        if result:
+            return {}, result
 
 
 class CharField(Field):
